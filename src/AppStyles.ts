b51a45e88ea4465671a60e18d9feba594379c346
Natalia Górska <nataliagorska27@gmail.com>
2021-09-25 23:33:58 +0200

    Adding css for game board

--- src/AppStyles.ts
+++ src/AppStyles.ts
@@ -7,12 +7,17 @@ export const AppWrapper = styled.div`
 `
 
 export const BoardWrapper = styled.div`
-    margin: 25px auto 0;
-    height: 80vh;
-    width: 60vw;
+    margin: 50px auto 0;
+    height: 85vh;
+    width: 57vw;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 `
 
 export const BoardGrid = styled.div`
+    height: 95%;
+    width: 95%;
+    margin: 0 auto;
+    padding-top: 13px;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-template-rows: repeat(3, 1fr);
