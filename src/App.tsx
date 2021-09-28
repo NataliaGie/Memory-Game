a947c7a108c7f57e0ffa72af83d332e639c24cf7
Natalia Górska <nataliagorska27@gmail.com>
2021-09-28 20:06:13 +0200

    game fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,47 +5,19 @@ import StartGameButton from "./components/StartGameButton/StartGameButton";
 import Modal from "./components/Modal/Modal";
 import Card from "./components/Card/Card";
 import CardPlaceholder from "./components/CardPlaceholder";
-import panda from './images/panda.jpg';
-import dog from './images/dog.jpg';
-import flamingo from './images/flamingo.jpg';
-import giraffe from './images/giraffe.jpg';
-import iguana from './images/iguana.jpg';
-import jaguar from './images/jaguar.jpg';
+import { animals, shuffleCards } from './utilities';
 
 function App() {
-
-  const animals = [
-    { img: panda, name: 'panda', flipped: false, matched: false, id: 1},
-    { img: dog, name: 'dog', flipped: false, matched: false, id: 2},
-    { img: flamingo, name: 'flamingo', flipped: false, matched: false, id: 3},
-    { img: giraffe, name: 'giraffe', flipped: false, matched: false, id: 4},
-    { img: jaguar, name: 'jaguar', flipped: false, matched: false, id: 5},
-    { img: iguana, name: 'iguana', flipped: false, matched: false, id: 6},
-    { img: panda, name: 'panda', flipped: false, matched: false, id: 7},
-    { img: dog, name: 'dog', flipped: false, matched: false, id: 8},
-    { img: flamingo, name: 'flamingo', flipped: false, matched: false, id: 9},
-    { img: giraffe, name: 'giraffe', flipped: false, matched: false, id: 10},
-    { img: jaguar, name: 'jaguar', flipped: false, matched: false, id: 12},
-    { img: iguana, name: 'iguana', flipped: false, matched: false, id: 11}
-  ];
 
   const [cards, setCards] = useState(animals);
   const [game, newGame] = useState(false);
   const [clickedCards, setClickedCards] = useState<any | null>([]);
   const [isModalOpen, setModalOpen] = useState(false);
 
-// Durstenfeld shuffle algorithm implementation
-  const shuffleCards = (array: any) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      let temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-      return array;
-    };
-
   const startingGameHandler = () => {
+    setCards(prevState => 
+      prevState.map(card => Object.assign({}, card, card.flipped = false)));
+    setClickedCards([]);
     setCards(shuffleCards(animals));
     newGame(true);
   };
